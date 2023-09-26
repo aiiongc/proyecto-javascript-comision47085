b59a2400e60be71260c5d630d7473829b7e08428
ignacio-herrera <iehp2013@gmail.com>
2023-09-26 19:22:44 -0300

    pre-entrega1+herrera

--- js/script.js
+++ js/script.js
@@ -1,28 +1,70 @@
 // Albums
-let album1 = "The Fame";
-let album2 = "Born this way";
-let album3 = "Artpop";
-let album4 = "Joanne";
-let album5 = "Chromatica";
+let album1 = "The Fame 20USD";
+let album2 = "Born this way 15USD";
+let album3 = "Artpop 15USD";
+let album4 = "Chromatica 30USD";
 
-
-
-
-let albums = prompt(
-        'Bienvenida/o a nuestra disquería. ¿Qué quieres escuchar hoy?: \n1 -' + album1 + '\n2 -' + album2 + '\n3 -' + album3 + '\n4 -' + album4 + '\n5 -' + album5
-    );
-switch (albums) {
-    case "1":
-        let mediosPago = prompt (
-            '¿Con que medio de pago pagas? \n1 - Debito \n2 - Credito'
-        );
-            switch (mediosPago) {
+let albums = prompt('Bienvenida/o a nuestra disquería. ¿Qué quieres escuchar hoy?: \n1 -' + album1 + '\n2 -' + album2 + '\n3 -' + album3 + '\n4 -' + album4);
+    switch (albums) {
+        case "1":
+            let mediosPago = prompt ('¿Con qué medio de pago pagas? \n1 - Debito \n2 - Credito');
+                switch (mediosPago) {
+                case "1":
+                    alert('¡Tu compra está casi lista!')
+                    let bancos = prompt('El total es 20 USD. Por favor selecciona a tu banco para completar el proceso \n1 - Banco Estado \n2 - Banco BBVA \n3 - Banco Santander');
+                    switch (bancos) {
+                        case "1": 
+                            login();
+                        case "2":
+                            login();
+                        case "3":
+                            login();
+                    }
+                case "2":
+                    alert('¡Tu compra está casi lista!');
+                    let bancos1 = prompt('El total es 15 USD. Por favor, selecciona a tu banco para completar el proceso \n1 - Banco Estado \n2 - Banco BBVA \n3 - Banco Santander');
+                    switch (bancos1) {
+                        case "1": 
+                            login();
+                        case "2":
+                            login();
+                        case "3":
+                            login();
+                    }
+                }
+        case "2":
+            let mediosPago2 = prompt ('¿Con qué medio de pago pagas? \n1 - Debito \n2 - Credito');
+                switch (mediosPago2) {
+                case "1":
+                    alert('¡Tu compra está casi lista!');
+                    let bancos2 = prompt('El total es 15 USD. Por favor selecciona a tu banco para completar el proceso \n1 - Banco Estado \n2 - Banco BBVA \n3 - Banco Santander');
+                    switch (bancos2) {
+                        case "1": 
+                            login();
+                        case "2":
+                            login();
+                        case "3":
+                            login();
+                    }
+                case "2":
+                    alert('¡Tu compra está casi lista!');
+                    let bancos22 = prompt('El total es 30 USD. Por favor, selecciona a tu banco para completar el proceso \n1 - Banco Estado \n2 - Banco BBVA \n3 - Banco Santander');
+                    switch (bancos22) {
+                        case "1": 
+                            login();
+                        case "2":
+                            login();
+                        case "3":
+                            login();
+                            }
+                }
+        case "3":
+            let mediosPago3 = prompt ('¿Con qué medio de pago pagas? \n1 - Debito \n2 - Credito');
+            switch (mediosPago3) {
             case "1":
                 alert('¡Tu compra está casi lista!')
-                let bancos =prompt(
-                    ' Por favor selecciona a tu banco para completar el proceso \n1 - Banco Estado \n2 - Banco BBVA \n3 - Banco Santander'
-                );
-                switch (bancos) {
+                let bancos3 = prompt('Por favor selecciona a tu banco para completar el proceso \n1 - Banco Estado \n2 - Banco BBVA \n3 - Banco Santander');
+                switch (bancos3) {
                     case "1": 
                         login();
                     case "2":
@@ -30,49 +72,52 @@ switch (albums) {
                     case "3":
                         login();
                 }
-            
             case "2":
-                switch (mediosPago) {
-                    case "1":
-                        alert('¡Tu compra está casi lista!')
-                        let bancos =prompt(
-                            ' Por favor selecciona a tu banco para completar el proceso \n1 - Banco Estado \n2 - Banco BBVA \n3 - Banco Santander'
-                        );
-                        switch (bancos) {
-                            case "1": 
-                                login();
-                            case "2":
-                                login();
-                            case "3":
-                                login();
+                alert('¡Tu compra está casi lista!');
+                let bancos33 = prompt('Por favor, selecciona a tu banco para completar el proceso \n1 - Banco Estado \n2 - Banco BBVA \n3 - Banco Santander');
+                switch (bancos33) {
+                    case "1": 
+                        login();
+                    case "2":
+                        login();
+                    case "3":
+                        login();
                         }
+            }
+        case "4":
+            let mediosPago4 = prompt ('¿Con qué medio de pago pagas? \n1 - Debito \n2 - Credito');
+                switch (mediosPago4) {
+                case "1":
+                    alert('¡Tu compra está casi lista!')
+                    let bancos4 = prompt('Por favor selecciona a tu banco para completar el proceso \n1 - Banco Estado \n2 - Banco BBVA \n3 - Banco Santander');
+                    switch (bancos4) {
+                        case "1": 
+                            login();
+                        case "2":
+                            login();
+                        case "3":
+                            login();
                     }
-                
-                
-        
-
-    case "2":
-        
-        
-
-    case "3":
-
-    case "4":
-
-    case "5":
+                case "2":
+                    alert('¡Tu compra está casi lista!');
+                    let bancos44 = prompt('Por favor, selecciona a tu banco para completar el proceso \n1 - Banco Estado \n2 - Banco BBVA \n3 - Banco Santander');
+                    switch (bancos44) {
+                        case "1": 
+                            login();
+                        case "2":
+                            login();
+                        case "3":
+                            login();
+                            }
+                }
     }
-}6666
 // login al banco
 function login(){
-
     const PIN = '1234';
     const solicitud = prompt ('Por favor ingresa tu PIN');
-    ;
-
     if (solicitud === PIN){
         alert('Gracias por comprar con nosotros.');
     }else{
         alert('Contraseña incorrecta');
-    } 
-
+    }
 }
